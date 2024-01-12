cfcf0411f2ba9759219d7a3bf330715a676e691c
kon1205 <131091843+kon1205@users.noreply.github.com>
2024-01-12 21:50:16 +0900

    Update ex1-1.js

--- ex1-1.js
+++ ex1-1.js
@@ -1,7 +1,14 @@
 function imgchange1(){
-  document.getElementById('logo').src='./image/siruetto.jpg';}
-function imgchange2(){
   document.getElementById('logo').src='./image/beima.png';}
+function imgchange2(){
+  document.getElementById('logo').src='./image/siruetto.jpg';}
 
+function txtchange1() {
+document.getElementById('txt1').innerHTML = '正解は、ベイマックス！';}
 
+function txtchange2() {
+document.getElementById('txt1').innerHTML = '正解は、メガ・ボット！';}
+
+function txtchange3() {
+document.getElementById('txt1').innerHTML = '正解は、ケアロボット！';}
 
